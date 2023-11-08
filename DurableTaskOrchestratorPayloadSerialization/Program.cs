12fc4dc684ccda2310abda8101a6d8591eedd2f3
anders <anders@filmweb.no>
2023-11-08 13:09:07 +0100

    Add JsonSerializerOptions with JsonNamingPolicy.CamelCase

--- DurableTaskOrchestratorPayloadSerialization/Program.cs
+++ DurableTaskOrchestratorPayloadSerialization/Program.cs
@@ -1,7 +1,20 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.Configure<JsonSerializerOptions>(options =>
+        {
+            options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            options.PropertyNameCaseInsensitive = true;
+            options.NumberHandling = JsonNumberHandling.AllowReadingFromString;
+            options.Converters.Add(new JsonStringEnumConverter());
+        });
+    })
     .Build();
 
 host.Run();
